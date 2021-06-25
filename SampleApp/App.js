03f56be138ae5197cd574a12e95c73713a2c639b
nohhow <xksrma97@gmail.com>
2021-06-26 02:47:23 +0900

    navigation tutorial

--- SampleApp/App.js
+++ SampleApp/App.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput, Button, TouchableOpacity, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
 
-const App = () => {
-
+function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>로그인</Text>
@@ -15,19 +17,88 @@ const App = () => {
         secureTextEntry={true}
         placeholder="비밀번호"
       />
-      <Button
-        style={styles.loginBtn}
-        title="로그인"
-        onPress={() => Alert.alert('Login Button pressed')}
-      />
-      <TouchableOpacity onPress={() => Alert.alert('Login Button pressed')}> 
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('Main')}
+      > 
         <Text style = {styles.loginBtn}>
-            로그인
+          로그인
         </Text>
-      </TouchableOpacity >
+      </TouchableOpacity>
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('SignUp')}
+      >
+        <Text style={styles.signUpBtn}>
+          회원가입
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-};
+}
+
+function MainScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>메인 페이지입니다.</Text>
+    </View>
+  );
+}
+
+function SignUpScreen({ navigation }) {
+  return (
+  <View style={styles.container}>
+      <Text style={styles.titleText}>회원가입</Text>
+      <View style={styles.row}>
+        <Text style={styles.text}>ID</Text>
+        <TextInput
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>PWD</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>CONFIRM</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Name</Text>
+        <TextInput
+          style={styles.input}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Main')}
+      >
+        <Text style={styles.loginBtn}>
+          회원가입
+        </Text>
+      </TouchableOpacity>
+  </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'   screenOptions={{headerShown: false}}>
+          <Stack.Screen name = 'Login' component={LoginScreen}/>
+          <Stack.Screen name = 'SignUp' component={SignUpScreen}/> 
+          <Stack.Screen name = 'Main' component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -50,12 +121,26 @@ const styles = StyleSheet.create({
   loginBtn: {
     height: 40,
     width : 200,
+    margin : 20,
     paddingTop : 5,
     fontSize: 20,
     backgroundColor: '#000',
     color : "#fff",
     textAlign: "center"
   },
+  signUpBtn: {
+    margin: 10,
+    fontSize: 15,
+  },
+  row: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text : {
+    width : '15%',
+  }
 });
 
 export default App;
